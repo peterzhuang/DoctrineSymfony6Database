25c6ca534a44db2ada8232e2613986ce5d019300
Yuchuan <yuchuan@example.com>
2022-12-01 13:54:47 -0500

    query single entity for 'Show' page

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,5 +31,15 @@ class MixController extends AbstractController
             $mix->getId(),
             $mix->getTrackCount()
         ));
+    }
+
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository)
+    {
+        $mix = $mixRepository->find($id);
+        
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
